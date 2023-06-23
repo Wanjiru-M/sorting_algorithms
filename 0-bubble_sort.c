06f9c9ac9596663823ce76b29ec4033be83cc0ee
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-23 04:33:54 -0700

    fix on check 4

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,31 +2,31 @@
 #include <stdio.h>
 
 /**
- * bubble_sort - sorts an array of integers in ascending order
- * @array: The array to be sorted
- * @size: number of elements in  the array
- */
+* bubble_sort - sorts an array of integers in ascending order
+* @array: The array to be sorted
+* @size: number of elements in  the array
+*/
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int pass, current;
-    int temp;
+unsigned int pass, current;
+int temp;
 
-    if (size < 2)
-        return;
+if (size < 2)
+return;
 
-    for (pass = 0; pass < size; pass++)
-    {
-        current = 0;
-        do
-        {
-            if (array[current] > array[current + 1])
-            {
-                temp = array[current];
-                array[current] = array[current + 1];
-                array[current + 1] = temp;
-                print_array(array, size);
-            }
-            current++;
-        } while (current < size - pass - 1);
-    }
+for (pass = 0; pass < size; pass++)
+{
+current = 0;
+do
+{
+if (array[current] > array[current + 1])
+{
+temp = array[current];
+array[current] = array[current + 1];
+array[current + 1] = temp;
+print_array(array, size);
+}
+current++;
+} while (current < size - pass - 1);
+}
 }

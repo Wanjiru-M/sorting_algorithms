aab0f80a41b3a2f9ab4deb4da97d786fb49eac90
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-28 03:43:58 -0700

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,6 @@
-#include "sort.h"
 #include <stdio.h>
+#include "sort.h"
 
-/**
- * swap - Swaps two integer values
- * @a: First integer
- * @b: Second integer
- */
 void swap(int *a, int *b)
 {
     int temp = *a;
@@ -13,62 +8,43 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-/**
- * partition - Lomuto partition scheme
- * @array: Array to be sorted
- * @low: Starting index
- * @high: Ending index
- * @size: Size of the array
- * Return: Partition index
- */
-int partition(int *array, int low, int high, size_t size)
+int lomuto_partition(int *array, int low, int high)
 {
     int pivot = array[high];
     int i = low - 1;
-    int j;
 
-    for (j = low; j <= high - 1; j++)
+    for (int j = low; j <= high - 1; j++)
     {
         if (array[j] < pivot)
         {
             i++;
             swap(&array[i], &array[j]);
+            print_array(array, high + 1);
         }
     }
 
     swap(&array[i + 1], &array[high]);
+    print_array(array, high + 1);
 
     return (i + 1);
 }
 
-/**
- * quicksort - Recursive function to perform quicksort
- * @array: Array to be sorted
- * @low: Starting index
- * @high: Ending index
- * @size: Size of the array
- */
-void quicksort(int *array, int low, int high, size_t size)
+void quick_sort_recursive(int *array, int low, int high)
 {
     if (low < high)
     {
-        int pi = partition(array, low, high, size);
+        int partition_index = lomuto_partition(array, low, high);
 
-        quicksort(array, low, pi - 1, size);
-        quicksort(array, pi + 1, high, size);
+        quick_sort_recursive(array, low, partition_index - 1);
+        quick_sort_recursive(array, partition_index + 1, high);
     }
 }
 
-/**
- * quick_sort - Sorts an array of integers in ascending order using Quick Sort
- * @array: The array to be sorted
- * @size: Number of elements in the array
- */
 void quick_sort(int *array, size_t size)
 {
     if (array == NULL || size < 2)
         return;
 
-    quicksort(array, 0, size - 1, size);
-    print_array(array, size);
+    quick_sort_recursive(array, 0, size - 1);
 }
+

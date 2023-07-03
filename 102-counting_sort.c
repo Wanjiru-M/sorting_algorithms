b807e280f50924f4a84539b5fbbb22c787b56b9f
wanjirumephis <wanjirunduhi@gmail.com>
2023-07-03 02:57:28 -0700

    update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -5,42 +5,43 @@ void counting_sort(int *array, size_t size) {
     if (size <= 1)
         return;
 
-    // Find the maximum value in the array
+    /* Find the maximum value in the array */
     int max = array[0];
-    for (size_t i = 1; i < size; i++) {
+    size_t i;
+    for (i = 1; i < size; i++) {
         if (array[i] > max)
             max = array[i];
     }
 
-    // Create a counting array of size max + 1
+    /* Create a counting array of size max + 1 */
     int *count = malloc((max + 1) * sizeof(int));
     if (count == NULL) {
         printf("Memory allocation failed!\n");
         return;
     }
 
-    // Initialize the counting array with zeros
-    for (int i = 0; i <= max; i++) {
+    /* Initialize the counting array with zeros */
+    for (i = 0; i <= max; i++) {
         count[i] = 0;
     }
 
-    // Count the occurrences of each number in the input array
-    for (size_t i = 0; i < size; i++) {
+    /* Count the occurrences of each number in the input array */
+    for (i = 0; i < size; i++) {
         count[array[i]]++;
     }
 
-    // Print the counting array
-    for (int i = 0; i <= max; i++) {
+    /* Print the counting array */
+    for (i = 0; i <= max; i++) {
         printf("%d, ", count[i]);
     }
     printf("\n");
 
-    // Modify the counting array to store the cumulative counts
-    for (int i = 1; i <= max; i++) {
+    /* Modify the counting array to store the cumulative counts */
+    for (i = 1; i <= max; i++) {
         count[i] += count[i - 1];
     }
 
-    // Create a temporary output array
+    /* Create a temporary output array */
     int *output = malloc(size * sizeof(int));
     if (output == NULL) {
         printf("Memory allocation failed!\n");
@@ -48,18 +49,18 @@ void counting_sort(int *array, size_t size) {
         return;
     }
 
-    // Build the sorted array
-    for (ssize_t i = size - 1; i >= 0; i--) {
-        output[count[array[i]] - 1] = array[i];
-        count[array[i]]--;
+    /* Build the sorted array */
+    for (ssize_t j = size - 1; j >= 0; j--) {
+        output[count[array[j]] - 1] = array[j];
+        count[array[j]]--;
     }
 
-    // Copy the sorted array back to the input array
-    for (size_t i = 0; i < size; i++) {
+    /* Copy the sorted array back to the input array */
+    for (i = 0; i < size; i++) {
         array[i] = output[i];
     }
 
-    // Free dynamically allocated memory
+    /* Free dynamically allocated memory */
     free(count);
     free(output);
 }
